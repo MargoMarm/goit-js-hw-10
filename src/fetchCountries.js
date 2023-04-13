8176fbb7aaae7aa9dd8cf2a4606da5507d5fde6e
MargoMarm <margogerasimenko@gmail.com>
2023-04-13 21:20:00 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,10 @@
 export default function fetchCountries(name) {
-	const fields = 'fields=name,capital,population,flags,languages'
-	return fetch(`https://restcountries.com/v3.1/name/${name}?${fields}`).then((data) =>  data.json())
-};
+	const fields = 'fields=name,capital,population,flags,languages';
+	
+	return fetch(`https://restcountries.com/v3.1/name/${name}?${fields}`).then(response => {
+		if (!response.ok) {
+			throw new Error(response.status);
+		}
+		return response.json()
+	});
+}
