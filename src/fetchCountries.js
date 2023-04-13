17752ed122a97d0eca678f8574e8cc2c36cf9558
MargoMarm <margogerasimenko@gmail.com>
2023-04-13 18:35:01 +0200

    add js

---
+++ src/fetchCountries.js
@@ -0,0 +1,3 @@
+export default function fetchCountries(name) {
+	return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then((data) =>  data.json())
+};
